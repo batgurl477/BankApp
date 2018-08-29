c2578794793f79e9b5abf9fa46a52463976719ff
Unknown <hannahelise477@hotmail.com>
2018-08-30 10:01:27 +1200

    Forgot Password link added and working.
    Forgot password and email confirmation both working with API and sending emails.

--- BankApp/BankApp/Startup.cs
+++ BankApp/BankApp/Startup.cs
@@ -27,6 +27,9 @@ namespace BankApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
