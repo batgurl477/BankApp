d237a5a280d8e96d8fea4774e4e70b0e151922ce
Unknown <hannahelise477@hotmail.com>
2018-10-06 18:04:05 +1300

    Transfer Money Between Clients Different Accounts

---
+++ BankApp/BankApp/Models/MoneyTransfer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApp.Models
+{
+    public class MoneyTransfer
+    {
+        public int ID { get; set; }
+        public double Amount { get; set; }
+        public string ToAccount { get; set; }
+        public string FromAccount { get; set; }
+        public string ClientRef { get; set; }
+        public string OtherRef { get; set; }
+        public ApplicationUser Client { get; set; }
+        public ClientBalance ClientBalance { get; set; }
+    }
+}
