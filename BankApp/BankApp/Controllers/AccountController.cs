02077308d485de0ba53b6bbf8bb062f143700f44
Unknown <hannahelise477@hotmail.com>
2018-11-19 10:01:40 +1300

    Register User Not Auto Signed In

--- BankApp/BankApp/Controllers/AccountController.cs
+++ BankApp/BankApp/Controllers/AccountController.cs
@@ -191,7 +191,6 @@ namespace BankApp.Controllers
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToAction("UserList", "Home");
                 }
